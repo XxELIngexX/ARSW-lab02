a47d3adf12888c20bff8a4ffd9daf20e70cfee4d
miguel-tellez <77862071+miguel-tellez@users.noreply.github.com>
2024-08-29 03:39:14 -0500

    actualizacion punto 2 MainCanodromo.java

--- parte2/src/main/java/arsw/threads/MainCanodromo.java
+++ parte2/src/main/java/arsw/threads/MainCanodromo.java
@@ -24,47 +24,60 @@ public class MainCanodromo {
 
                     @Override
                     public void actionPerformed(final ActionEvent e) {
-						//como acción, se crea un nuevo hilo que cree los hilos
-                        //'galgos', los pone a correr, y luego muestra los resultados.
-                        //La acción del botón se realiza en un hilo aparte para evitar
-                        //bloquear la interfaz gráfica.
+                        // como acción, se crea un nuevo hilo que cree los hilos
+                        // 'galgos', los pone a correr, y luego muestra los resultados.
+                        // La acción del botón se realiza en un hilo aparte para evitar
+                        // bloquear la interfaz gráfica.
                         ((JButton) e.getSource()).setEnabled(false);
                         new Thread() {
                             public void run() {
                                 for (int i = 0; i < can.getNumCarriles(); i++) {
-                                    //crea los hilos 'galgos'
+                                    // crea los hilos 'galgos'
                                     galgos[i] = new Galgo(can.getCarril(i), "" + i, reg);
-                                    //inicia los hilos
+                                    // inicia los hilos
                                     galgos[i].start();
 
                                 }
-                               
-				can.winnerDialog(reg.getGanador(),reg.getUltimaPosicionAlcanzada() - 1); 
+                                for (Galgo galgo : galgos) {
+                                    try {
+                                        galgo.join();
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+
+                                can.winnerDialog(reg.getGanador(), reg.getUltimaPosicionAlcanzada() - 1);
                                 System.out.println("El ganador fue:" + reg.getGanador());
                             }
                         }.start();
 
                     }
-                }
-        );
+                });
 
         can.setStopAction(
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
+                        for (int i = 0; i < can.getNumCarriles(); i++) {
+                            galgos[i].keepgoing(false);
+                        }
                         System.out.println("Carrera pausada!");
                     }
-                }
-        );
+                });
 
         can.setContinueAction(
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
+                        for (int i = 0; i < can.getNumCarriles(); i++) {
+                            galgos[i].keepgoing(true);
+                        }
+                        synchronized (reg) {
+                            reg.notifyAll();
+                        }
                         System.out.println("Carrera reanudada!");
                     }
-                }
-        );
+                });
 
     }
 
